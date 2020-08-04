4c865efa929314b2acd58cac08bc37599b91cc8c
nitinsjiloha <jilohanitin@gmail.com>
2020-08-05 00:10:05 +0530

    ADD Delete fn and Profile Page

--- fullsite/polls/urls.py
+++ fullsite/polls/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
     path('signout', views.signout, name='signout'),
-    path('userlist', views.user_list, name='userlist'),]
+    path('delete', views.delete, name='delete'),]
--- fullsite/polls/views.py
+++ fullsite/polls/views.py
@@ -1,12 +1,16 @@
+import logging
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout
 from .models import users
+LOG_FILENAME = 'loginfo.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
 # Create your views here.
 
-
+@csrf_exempt
 def signup(request):
 
     if request.method == 'POST':
@@ -17,36 +21,46 @@ def signup(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             users.objects.create(user=user)
+            logging.info("New User Signed Up {}".format(username))
             login(request, user)
-            return render( request, 'userlist.html')
+            return render( request, 'signin.html')
         else:
             return render(request, 'signup.html', {'form': form})
     else:
         return render(request, 'signup.html')
 
 
-
+@csrf_exempt
 def signin(request):
-    if request.user.is_authenticated:
-        return render(request, 'userlist.html')
+    # if request.user.is_authenticated:
+    #     return render(request, 'userlist.html')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-
-            return render( request, 'userlist.html')
+            all_user = users.objects.all()
+            if all_user is not None:
+                return render(request, "userlist.html", {'results': all_user})
+            else:
+                return render(request, 'signup.html')
         else:
-
             return render(request, 'signup.html')
     else:
         return render(request, 'signin.html')
 
+@csrf_exempt
 def signout(request):
     logout(request)
     return redirect('signin')
 
-def user_list(request):
-    all_user = users.objects.all()
-    return render (request, "userlist.html")
+
+def delete(request):
+    id = request.POST['userID']
+    if request.method == "POST":
+        obj = users.objects.filter(id=id)
+        obj.delete()
+        all_user = users.objects.all()
+        return render("userlist.html", {'results': all_user})
+    return render(request, "signin.html")
